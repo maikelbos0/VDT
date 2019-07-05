38f50da74e786ca01aa4cafd92df9166b7a09b47
Maikel Bos <maikel.bos0@gmail.com>
2019-07-05 15:33:39 +0200

    Trying to extend jQuery

---
+++ Playground/Scripts/jquery.dropdown.js
@@ -0,0 +1,15 @@
+﻿(function ($) {
+    $.fn.dropdown = function (settings) {
+        var options = $.extend({}, $.fn.dropdown.defaults, settings);
+
+        console.log(options);
+
+        return this;
+    };
+
+    $.fn.dropdown.defaults = {
+        multiselect: false,
+        valueAttribute: "value",
+        selectedAttribute: "selected"
+    }
+}(jQuery));
