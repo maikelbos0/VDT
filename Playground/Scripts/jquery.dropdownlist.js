8ab9362b6a3d92880b971e67835f2bf960b85d9c
Maikel Bos <maikel.bos0@gmail.com>
2019-07-06 12:32:27 +0200

    Test for trying to add function to dropdown

--- Playground/Scripts/jquery.dropdownlist.js
+++ Playground/Scripts/jquery.dropdownlist.js
@@ -1,43 +1,102 @@
 ﻿(function ($) {
+    // Keep track of existing dropdownlists
+    let dropdownlists = [];
+
+    // Create extension
     $.fn.dropdownlist = function (settings) {
-        let options = $.extend({}, $.fn.dropdownlist.defaults, settings);
-
         return $(this).each(function () {
-            let element = $(this);
-            let list = $('<div>', { class: "dropdownlist" });
-            let listSelector = $('<div>', { class: "dropdownlist-selector" }).append(
-                $('<div>', { class: "dropdownlist-selector-text" }).text(options.getEmptyText()),
-                $('<div>', { class: "dropdownlist-selector-toggle" })
-            );
-
-            $(element).before(list);
-            list.append(listSelector);
-
-            let listList = $('<div>', { class: "dropdownlist-list" }).append(element);
-            listList.hide();
-            list.append(listList);
-            
-            options.initialization.getItems(this).each(function () {
-                console.log("- " + options.getItemValue(this));
+            let existingLists = dropdownlists.filter(function (list) {
+                return list.element.is($(this));
             });
+
+            if (existingLists.length > 0) {
+                debugger;
+                let dropdownlist = existingLists[0];
+
+                if (typeof settings === 'string' && settings in dropdownlist) {
+                    // We can't return a value here but we want to.
+                }
+            }
+            else {
+                let options = $.extend({}, $.fn.dropdownlist.defaults, settings);
+                let dropdownlist = new Dropdownlist($(this), options);
+
+                dropdownlist.initialize();
+                dropdownlists.push(dropdownlist);
+            }
         });
     };
 
+    // Set defaults for extension
     $.fn.dropdownlist.defaults = {
         initialization: {
             getItems: function (element) {
                 return $(element).children();
             },
             isItemSelected: function (item) {
-                return $(item).data("selected") !== undefined && $(item).data("selected") !== "false";
+                return $(item).data('selected') !== undefined && $(item).data('selected') !== 'false';
             }
         },
         getItemValue: function (item) {
-            return $(item).data("value") || $(item).text()
+            return $(item).data('value') || $(item).text()
         },
         getEmptyText: function () {
             return '(Select...)'
         },
         multiselect: false
     }
+
+    // Dropdownlist implementation
+    function Dropdownlist(element, options) {
+        this.element = element;
+        this.options = options;
+        this.list = null;
+        this.container = null;
+        this.selector = null;
+    }
+
+    Dropdownlist.prototype.destroy = function () {
+        this.container.before(this.element);
+        this.container.destroy();
+    }
+
+    Dropdownlist.prototype.test = function () {
+        return 'test';
+    }
+
+    Dropdownlist.prototype.initialize = function () {
+        // Alias for event handlers
+        let base = this;
+
+        // Add container early so can move the element after without issues
+        this.container = $('<div>', { class: 'dropdownlist' });
+        this.element.before(this.container);
+
+        // Select element
+        this.selector = $('<div>', { class: 'dropdownlist-selector' }).append(
+            $('<div>', { class: 'dropdownlist-selector-text' }).text(this.options.getEmptyText()),
+            $('<div>', { class: 'dropdownlist-selector-toggle' })
+        );
+        this.selector.click(function () {
+            base.toggle();
+        });
+
+        // List container
+        this.list = $('<div>', { class: 'dropdownlist-list' }).append(this.element).hide();
+
+        // Final assembly
+        this.container.append(this.selector);
+        this.container.append(this.list);
+    }
+
+    Dropdownlist.prototype.toggle = function () {
+        this.list.toggle();
+    }
+
+    /*
+     * TODO
+     * - Checkboxes
+     * - Event handlers
+     * - ?
+     */
 }(jQuery));
