a38bedb45a3028bef22d02da1c7b825c3c2e7f2d
Maikel Bos <maikel.bos0@gmail.com>
2019-07-06 18:11:58 +0200

    Add fields, add selector text

--- Playground/Scripts/jquery.dropdownlist.js
+++ Playground/Scripts/jquery.dropdownlist.js
@@ -34,26 +34,41 @@
 
     // Set defaults for extension
     $.fn.dropdownlist.defaults = {
-        initialization: {
-            getItems: function (element) {
-                return $(element).children();
-            },
-            isItemSelected: function (item) {
-                return $(item).data('selected') !== undefined && $(item).data('selected') !== 'false';
-            },
-            fieldName: null,
-            getFieldName: function (element) {
-                return $(element).data('field-name');
-
-            }
+        // Items are all the possible dropdown items to select
+        // Defaults to all direct children of the element
+        getItems: function (element) {
+            return $(element).children();
         },
+        // Determine if an item should be selected during initialization
+        // Multiple selected items for a single-select dropdownlist can have unexpected side effects
+        isItemSelected: function (item) {
+            return $(item).data('selected') !== undefined && $(item).data('selected') != false;
+        },
+        // The field name to use for the generated input fields
+        // Defaults to the data-property field-name
+        getFieldName: function (element) {
+            return $(element).data('field-name');
+        },
+        // The field value to use for the generated input fields based on the item
+        // Defaults to the data-property value of the item
         getItemValue: function (item) {
-            return $(item).data('value') || $(item).text()
+            return $(item).data('value') || $(item).text();
         },
+        // The text to get for an item based on the item
+        // Defaults to the text-content of the item
+        getItemText: function (item) {
+            return $(item).text();
+        },
+        // The text to display when no items are selected
+        // Override this implementation to set the text or provide multi-language support
         getEmptyText: function () {
-            return '(Select...)'
+            return '(Select...)';
         },
-        multiselect: false
+        // Multiselect dropdowns use checkboxes, single select uses an invisible radio button
+        // Defaults to false except when the data-property multiselect is provided
+        isMultiselect: function (element) {
+            return $(element).data('multiselect') !== undefined && $(element).data('multiselect') != false;
+        }
     }
 
     // Extension for getting a dropdownlist object for access; only supports retrieving 1
@@ -67,6 +82,7 @@
 
         this.element = element;
         this.options = options;
+        this.fieldName = this.options.getFieldName(this.element) || "dropdownlist-" + Math.random().toString().replace('.', '');
 
         // Add container early so can move the element after without issues
         this.container = $('<div>', { class: 'dropdownlist' });
@@ -74,7 +90,7 @@
 
         // Select element
         this.selector = $('<div>', { class: 'dropdownlist-selector' }).append(
-            $('<div>', { class: 'dropdownlist-selector-text' }).text(this.options.getEmptyText()),
+            $('<div>', { class: 'dropdownlist-selector-text' }),
             $('<div>', { class: 'dropdownlist-selector-toggle' })
         );
 
@@ -82,20 +98,60 @@
         this.list = $('<div>', { class: 'dropdownlist-list' }).append(this.element).hide();
 
         // Add input fields
-        let fieldName = this.options.initialization.fieldName || this.options.initialization.getFieldName(this.element) || "dropdownlist-" + dropdownlists.items.length;
-        console.log(fieldName);
+        this.options.getItems(this.element).each(function () {
+            let fieldProperties = {
+                name: base.fieldName,
+                value: base.options.getItemValue($(this))
+            };
+
+            if (base.options.isMultiselect(base.element)) {
+                fieldProperties.type = 'checkbox';
+                fieldProperties.class = 'dropdownlist-field';
+            }
+            else {
+                fieldProperties.type = 'radio';
+                fieldProperties.class = 'dropdownlist-field dropdownlist-field-hidden';
+            }
+
+            if (base.options.isItemSelected($(this))) {
+                fieldProperties.checked = 'true';
+            }
+
+            $(this).prepend($('<input>', fieldProperties));
+        });
 
         // Final assembly
         this.container.append(this.selector);
         this.container.append(this.list);
+        this.setSelectorText();
 
         // Event handlers
         this.selector.click(function (e) {
             base.toggle();
         });
 
-        this.options.initialization.getItems(this.element).click(function (e) {
-            console.log('Chose an item: ' + base.options.getItemValue(e.target));
+        this.list.click(function (e) {
+            let item = $(e.target).closest('.dropdownlist-list > * > *');
+
+            // Only bother selecting/unselecting when clicking an item
+            if (item.length === 0) {
+                return;
+            }
+
+            let input = item.find('input.dropdownlist-field');
+
+            // Let the input field handle the actual click
+            if (!input.is(e.target)) {
+                input.click();
+            }
+            else {
+                // Actual click handling
+                base.setSelectorText();
+
+                if (!base.options.isMultiselect(base.element)) {
+                    base.hide();
+                }
+            }
         });
 
         $(document).click(function (e) {
@@ -110,6 +166,7 @@
     Dropdownlist.prototype.remove = function () {
         this.container.before(this.element);
         this.container.remove();
+        this.options.getItems(this.element).find("input.dropdownlist-field").remove();
 
         dropdownlists.remove(this);
     }
@@ -126,10 +183,29 @@
         this.list.hide();
     }
 
+    Dropdownlist.prototype.setSelectorText = function () {
+        let items = this.getSelectedItems();
+        let text = this.options.getEmptyText();
+
+        if (items.length > 0) {
+            text = $.map(items, this.options.getItemText).join(', ');
+        }
+
+        this.container.find('.dropdownlist-selector-text').text(text);
+    }
+
+    Dropdownlist.prototype.getSelectedItems = function () {
+        return this.options.getItems(this.element).has('input:checked');
+    }
+
+    Dropdownlist.prototype.getSelectedValues = function () {
+        return $.map(this.getSelectedItems(), this.options.getItemValue);
+    }
+
     /*
      * TODO
-     * - Checkboxes
      * - Event handlers
+     * - Implement set of selected items
      * - ?
      */
 }(jQuery));
