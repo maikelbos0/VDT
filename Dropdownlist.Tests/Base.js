8c4e965ea5f599d12e7ee2ac5a54263d053c69f7
Maikel Bos <maikel.bos0@gmail.com>
2019-07-10 20:24:40 +0200

    Basic tests

--- Dropdownlist.Tests/Base.js
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="Base.html" />
-
-describe("a basic dropdownlist", function () {
-    it('can be created', function () {
-        expect(function () {
-            $('#base-dropdown').dropdownlist();
-        }).not.toThrow();
-    });
-});
---
+++ Dropdownlist.Tests/Basic.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="Basic.html" />
+
+describe("a basic dropdownlist", function () {
+    it('can be created', function () {
+        var dropdown = $('#base-dropdown');
+
+        expect(dropdown.length).toEqual(1);
+
+        expect(function () {
+            dropdown.dropdownlist();
+        }).not.toThrow();
+    });
+
+    it('creates fields with the correct name', function () {
+        $('#base-dropdown').dropdownlist();
+
+        var fields = $('#base-dropdown').find('input.dropdownlist-field[name="dropdown-value"]');
+
+        expect(fields.length).toEqual(3);
+    });
+
+    it('is single-select by default', function () {
+        $('#base-dropdown').dropdownlist();
+
+        var fields = $('#base-dropdown').find('input.dropdownlist-field[type="radio"]');
+
+        expect(fields.length).toEqual(3);
+    });
+
+    it('in single-select mode selects the first option by default', function () {
+        $('#base-dropdown').dropdownlist();
+
+        var field = $('#base-dropdown').find('input.dropdownlist-field').first();
+
+        expect(field.prop('checked')).toEqual(true);
+    });
+
+    it('has the correct field values', function () {
+        $('#base-dropdown').dropdownlist();
+
+        var fields = $('#base-dropdown').find('input.dropdownlist-field');
+
+        expect($(fields[0]).val()).toEqual('1');
+        expect($(fields[1]).val()).toEqual('2');
+        expect($(fields[2]).val()).toEqual('Final option');
+    });
+});
