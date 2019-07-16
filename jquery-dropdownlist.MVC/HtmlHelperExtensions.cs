54942a4be251691e5fb30ebaca4708007270fad1
Maikel Bos <maikel.bos0@gmail.com>
2019-07-16 20:08:46 +0200

    Html extension - add overloads and start building tags

--- jquery-dropdownlist.MVC/HtmlHelperExtensions.cs
+++ jquery-dropdownlist.MVC/HtmlHelperExtensions.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
 
 namespace vdt.jquerydropdownlist.MVC {
     public static class HtmlHelperExtensions {
-        public static IHtmlString JQueryDropdownlistFor<TModel>(this HtmlHelper<TModel> html, Expression<Func<TModel, string>> expression) {
-            return new MvcHtmlString(expression.Compile().Invoke(html.ViewData.Model));
+        public static MvcHtmlString JQueryDropdownlistFor<TModel>(this HtmlHelper<TModel> html,
+                                                                  Expression<Func<TModel, string>> expression) {
+            return JQueryDropdownlistFor(html, expression, null);
+        }
+
+        public static MvcHtmlString JQueryDropdownlistFor<TModel>(this HtmlHelper<TModel> html,
+                                                                  Expression<Func<TModel, string>> expression,
+                                                                  object htmlAttributes) {
+            return JQueryDropdownlistFor(html, expression, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+        public static MvcHtmlString JQueryDropdownlistFor<TModel>(this HtmlHelper<TModel> html,
+                                                                  Expression<Func<TModel, string>> expression,
+                                                                  IDictionary<string, object> htmlAttributes) {
+            var builder = new TagBuilder("div");
+            var id = ExpressionHelper.GetExpressionText(expression);
+
+            if (!string.IsNullOrWhiteSpace(id)) {
+                builder.MergeAttribute("id", TagBuilder.CreateSanitizedId(id));
+            }
+            
+            builder.SetInnerText(expression.Compile().Invoke(html.ViewData.Model));
+            builder.MergeAttributes(htmlAttributes, true);
+
+            return new MvcHtmlString(builder.ToString());
         }
     }
 }
