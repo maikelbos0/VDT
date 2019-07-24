1e9345904fe935a66761b439005c2b84f1590eaf
Maikel Bos <maikel.bos0@gmail.com>
2019-07-24 19:12:59 +0200

    Added tests project for datagridview

---
+++ jquery-datagridview.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("vdt.jquery-datagridview.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © 2019 Maikel bos")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70c64d35-a6a0-4669-b328-f179b23ad295")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ jquery-datagridview.tests/basic.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="basic.html" />
+
+describe('a basic datagridview', function () {
+    it('can be tested', function () {
+        expect(test).toEqual(1);
+    });
+});
