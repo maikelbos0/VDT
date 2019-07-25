8746ac4c58fbe0350af8e4d4264fae732f1c1fdb
Maikel Bos <maikel.bos0@gmail.com>
2019-07-25 19:16:11 +0200

    Basic plugin setup

--- jquery-datagridview/Scripts/jquery-datagridview.js
+++ jquery-datagridview/Scripts/jquery-datagridview.js
@@ -1,1 +1,48 @@
 ﻿var test = 1;
+
+(function ($) {
+
+    // Extension for creating datagridviews; supports multiple creations in one call
+    $.fn.datagridview = function (settings, callback) {
+        // Allow callback to be the only argument
+        if ($.isFunction(settings)) {
+            callback = settings;
+            settings = null;
+        }
+
+        return $(this).each(function () {
+            let datagridview;
+
+            if (!$(this).data('datagridview')) {
+                let options = $.extend({}, $.fn.datagridview.defaults, settings);
+                datagridview = new DataGridView($(this), options);
+
+                // Add object to data
+                $(this).data('datagridview', datagridview);
+            }
+            else {
+                // Get object from data
+                datagridview = $(this).data('datagridview');
+            }
+
+            // Call the callback, bound to the datagridview
+            if ($.isFunction(callback)) {
+                callback.bind(datagridview)(datagridview);
+            }
+        });
+    }
+
+    // Set defaults for extension
+    $.fn.datagridview.defaults = {}
+
+    // Datagridview implementation
+    function DataGridView(element, options) {
+        this.element = element;
+        this.options = options;
+    }
+
+    // Event handlers should not be accessible from the object itself
+    let eventHandlers = {
+    }
+
+}(jQuery));
