6eefe6915d52e48aa6bc5bc1f3fc0530f9a7d2ef
Maikel Bos <maikel.bos0@gmail.com>
2019-07-30 19:53:52 +0200

    Data from controller

--- demo/Controllers/DatagridviewController.cs
+++ demo/Controllers/DatagridviewController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using vdt.demo.Models;
 
 namespace vdt.demo.Controllers {
     [RoutePrefix("Datagridview")]
@@ -7,6 +10,50 @@ namespace vdt.demo.Controllers {
         [Route("Index")]
         public ActionResult Index() {
             return View();
+        }
+
+        [HttpPost]
+        [Route("GetInvoiceGridItems")]
+        public JsonResult GetInvoiceGridItems() {
+            return Json(GenerateInvoiceGridItems().Take(25));
+        }
+
+        public IEnumerable<InvoiceGridItemViewModel> GenerateInvoiceGridItems() {
+            var currencies = new[] { "EUR", "GBP", "SEK", "USD" };
+            var i = 0;
+
+            while (true) {
+                yield return new InvoiceGridItemViewModel() {
+                    DebtorNumber = $"A{(i / 5) + 123:000000}",
+                    DebtorName = GenerateName(i),
+                    InvoiceNumber = $"{i + 455:00000000}",
+                    InvoiceDate = new System.DateTime(2019, 1, 1).AddDays(i),
+                    DueDate = new System.DateTime(2019, 1, 31).AddDays(i),
+                    Currency = currencies[i % currencies.Length],
+                    InvoiceAmount = 123.45m + (3 - i % 5) * 23 + (i % 223 * 1.03m),
+                    InvoiceOpenAmount = 123.45m + (5 - i % 7) * 23 + (i % 223 * 1.13m)
+                };
+
+                i++;
+            }
+        }
+
+        public string GenerateName(int i) {
+            var wordLengthMods = new[] { 7, 3, 5, 3 };
+            var wordCount = (i + 3) % 4 + 1;
+            var nameBuilder = new System.Text.StringBuilder();
+
+            for (var w = 0; w < wordCount; w++) {
+                nameBuilder.Append((char)('A' + (i * 3 + w * 7) % 26));
+
+                for (var c = 0; c < i % wordLengthMods[w] + 1; c++) {
+                    nameBuilder.Append((char)('a' + (i * 11 + w * 5 + c * 17) % 26));
+                }
+
+                nameBuilder.Append(" ");
+            }
+
+            return nameBuilder.ToString().Trim(' ');
         }
     }
 }
---
+++ demo/Models/InvoiceGridItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace vdt.demo.Models {
+    public class InvoiceGridItemViewModel {
+        public string DebtorNumber { get; set; }
+        public string DebtorName { get; set; }
+        public string InvoiceNumber { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public string InvoiceDateString { get { return InvoiceDate.ToShortDateString(); } }
+        public DateTime DueDate { get; set; }
+        public string DueDateString { get { return DueDate.ToShortDateString(); } }
+        public string Currency { get; set; }
+        public decimal InvoiceAmount { get; set; }
+        public string InvoiceAmountString { get { return InvoiceAmount.ToString("N2"); } }
+        public decimal InvoiceOpenAmount { get; set; }
+        public string InvoiceOpenAmountString { get { return InvoiceOpenAmount.ToString("N2"); } }
+    }
+}
