6ccfe0b4e4432ef3bd5038b1407483bc69d7b56e
Maikel Bos <maikel.bos0@gmail.com>
2019-08-08 21:06:40 +0200

    Datagridview options tests implementation

--- jquery-datagridview.tests/options.js
+++ jquery-datagridview.tests/options.js
@@ -2,10 +2,38 @@
 
 describe('a datagridview option', function () {
     it('can be provided for getMetaData', function () {
-        fail();
+        var metaData;
+
+        $('#datagridview-options-meta-data').datagridview({
+            columns: [],
+            getMetaData: function () {
+                return new DataGridViewMetaData('test', false, 0, 25, 0);
+            }
+        }, function () {
+                metaData = this.getMetaData();
+        });
+
+        expect(metaData.sortColumn).toEqual('test');
     });
 
     it('can be provided for getFooterPlugins', function () {
-        fail();
+        var grid = $('#datagridview-options-footer-plugins');
+
+        grid.datagridview({
+            columns: [{ data: 'test' }],
+            getFooterPlugins: function () {
+                return [
+                    $.fn.datagridview.footerPlugins.prevNext,
+                    $.fn.datagridview.footerPlugins.displayFull
+                ];
+            }
+        }, function () {
+            this.populate(new DataGridViewMetaData('test', false, 2, 25, 0), [
+                { test: '1' },
+                { test: '2' }
+            ])
+        });
+
+        expect(grid.find('.datagridview-footer-element').length).toEqual(2);
     });
 });
