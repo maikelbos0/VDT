ea4daf372613cd8d654317479ed891fd6e2864dd
Maikel Bos <maikel.bos0@gmail.com>
2019-08-11 09:33:11 +0200

    Initial drag tests

--- jquery-datagridview.tests/dragging.js
+++ jquery-datagridview.tests/dragging.js
@@ -2,26 +2,52 @@
 
 describe('dragging datagridview', function () {
     it('headers is disabled by default', function () {
-        fail();
+        var grid = $('#datagridview-dragging-disabled');
+
+        grid.datagridview({
+            columns: [
+                { data: 'test' },
+                { data: 'column' }
+            ]
+        });
+
+        expect(grid.find('.datagridview-header-drag').length).toEqual(0);
     });
 
     it('headers can be enabled', function () {
-        fail();
+        var grid = $('#datagridview-dragging-enabled');
+
+        grid.datagridview({
+            columns: [
+                { data: 'test' },
+                { data: 'column' }
+            ]
+        });
+
+        expect(grid.find('.datagridview-header-drag').length).toEqual(2);
     });
 
     it('header changes the size of the adjacent headers', function () {
+        var grid = $('#datagridview-dragging-adjacent-headers');
+
         fail();
     });
 
     it('header does not change the size of other headers', function () {
+        var grid = $('#datagridview-dragging-other-headers');
+
         fail();
     });
 
     it('last header changes only the size of the last header', function () {
+        var grid = $('#datagridview-dragging-last-header');
+
         fail();
     });
 
     it('header does not cause sorting on the header', function () {
+        var grid = $('#datagridview-dragging-no-sorting');
+
         fail();
     });
 });
