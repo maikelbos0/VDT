499e93eac16090130eb5622d25cb38b9a69544c3
Maikel Bos <maikel.bos0@gmail.com>
2019-08-17 14:19:44 +0200

    Multiselect tests

--- jquery-datagridview.tests/selection.js
+++ jquery-datagridview.tests/selection.js
@@ -168,15 +168,87 @@ describe('in a datagridview a user can select', function () {
     });
 
     it('a range of rows after selecting a higher row by shift+clicking on a lower row in multiselect mode', function () {
-        fail();
+        var grid = $('#selection-multi-shift');
+
+        grid.datagridview({
+            columns: [
+                { data: 'test' }
+            ]
+        }, function () {
+            this.populate(this.getMetaData(), [
+                { test: 1, },
+                { test: 2, },
+                { test: 3, },
+                { test: 4, },
+                { test: 5, } 
+            ]);
+        });
+
+        triggerMouseEvent(grid.find('.datagridview-row:nth-child(2)'), 'mouseup');
+        triggerMouseEvent(grid.find('.datagridview-row:nth-child(4)'), 'mouseup', false, true);
+
+        var rows = grid.find('.datagridview-row-selected');
+
+        expect(rows.length).toEqual(3);
+        expect(rows[0]).toEqual(grid.find('.datagridview-row')[1]);
+        expect(rows[2]).toEqual(grid.find('.datagridview-row')[3]);
     });
 
     it('a range of rows after selecting a lower row by shift+clicking on a higher row in multiselect mode', function () {
-        fail();
+        var grid = $('#selection-multi-shift-reverse');
+
+        grid.datagridview({
+            columns: [
+                { data: 'test' }
+            ]
+        }, function () {
+            this.populate(this.getMetaData(), [
+                { test: 1, },
+                { test: 2, },
+                { test: 3, },
+                { test: 4, },
+                { test: 5, }
+            ]);
+        });
+
+        triggerMouseEvent(grid.find('.datagridview-row:nth-child(4)'), 'mouseup');
+        triggerMouseEvent(grid.find('.datagridview-row:nth-child(2)'), 'mouseup', false, true);
+
+        var rows = grid.find('.datagridview-row-selected');
+
+        expect(rows.length).toEqual(3);
+        expect(rows[0]).toEqual(grid.find('.datagridview-row')[1]);
+        expect(rows[2]).toEqual(grid.find('.datagridview-row')[3]);
     });
 
     it('an additional range of rows by ctrl+clicking and ctrl+shift+clicking in multiselect mode', function () {
-        fail();
+        var grid = $('#selection-multi-ctrl-shift');
+
+        grid.datagridview({
+            columns: [
+                { data: 'test' }
+            ]
+        }, function () {
+            this.populate(this.getMetaData(), [
+                { test: 1, },
+                { test: 2, },
+                { test: 3, },
+                { test: 4, },
+                { test: 5, },
+                { test: 6, }
+            ]);
+
+            this.setSelectedIndexes([0, 1]);
+        });
+
+        triggerMouseEvent(grid.find('.datagridview-row:nth-child(4)'), 'mouseup', true);
+        triggerMouseEvent(grid.find('.datagridview-row:nth-child(5)'), 'mouseup', true, true);
+
+        var rows = grid.find('.datagridview-row-selected');
+
+        expect(rows.length).toEqual(4);
+        expect(rows[0]).toEqual(grid.find('.datagridview-row')[0]);
+        expect(rows[3]).toEqual(grid.find('.datagridview-row')[4]);
     });
 
     it('a range of rows by dragging from a higher row to a lower row in multiselect mode', function () {
