88daa5339b3ab60da9741ab73c0ba5f86c5d5ebe
Maikel Bos <maikel.bos0@gmail.com>
2019-09-01 17:33:25 +0200

    Cleanup

--- demo/App_Start/BundleConfig.cs
+++ demo/App_Start/BundleConfig.cs
@@ -1,9 +1,7 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace vdt.demo.App_Start {
     public class BundleConfig {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/Scripts/jquery").Include(
                 "~/Scripts/jquery-3.3.1.min.js",
