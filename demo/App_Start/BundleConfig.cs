c192fc561891798b683d5af448b35fd0315b82aa
Maikel Bos <maikel.bos0@gmail.com>
2020-01-21 18:58:48 +0100

    Extract required style from optional

--- demo/App_Start/BundleConfig.cs
+++ demo/App_Start/BundleConfig.cs
@@ -16,6 +16,7 @@ namespace vdt.demo.App_Start {
                 "~/Content/bootstrap.min.css",
                 "~/Content/Site.css",
                 "~/../jquery-dropdownlist/Content/jquery-dropdownlist.css",
+                "~/../jquery-dropdownlist/Content/jquery-dropdownlist.style.css",
                 "~/../jquery-datagridview/Content/jquery-datagridview.css",
                 "~/../jquery-datagridview/Content/jquery-datagridview.style.css",
                 "~/../jquery-rangeslider/Content/jquery-rangeslider.css",
