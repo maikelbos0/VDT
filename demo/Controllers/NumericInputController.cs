bd7831d19d65332e6bacd2314b90f5adb1b9011a
Maikel Bos <maikel.bos0@gmail.com>
2020-02-06 16:36:57 +0100

    Numeric input proof of concept

---
+++ demo/Controllers/NumericInputController.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Web.Mvc;
+
+namespace vdt.demo.Controllers {
+    [RoutePrefix("NumericInput")]
+        public class NumericInputController : Controller {
+        [Route]
+        [Route("Index")]
+        public ActionResult Index() {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("as-IN");
+
+            return View();
+        }
+    }
+}
