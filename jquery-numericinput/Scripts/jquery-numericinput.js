c5c9ba21a0555e0db92500fffeb8fee27212dbb2
Maikel Bos <maikel.bos0@gmail.com>
2020-02-07 17:48:00 +0100

    Basic jquery plugin setup

--- jquery-numericinput/Scripts/jquery-numericinput.js
+++ jquery-numericinput/Scripts/jquery-numericinput.js
@@ -1,1 +1,49 @@
-﻿alert('asdf');
+﻿(function ($) {
+
+    // Extension for creating numeric input; supports multiple creations in one call
+    $.fn.numericinput = function (settings, callback) {
+        // Allow callback to be the only argument
+        if ($.isFunction(settings)) {
+            callback = settings;
+            settings = null;
+        }
+
+        return $(this).each(function () {
+            // Get object from data
+            let numericinput = $(this).data('numericinput');
+
+            if (!numericinput) {
+                // Create object
+                let options = $.extend({}, $.fn.numericinput.defaults, settings);
+                numericinput = new Numericinput($(this), options);
+
+                // Add object to data
+                $(this).data('numericinput', numericinput);
+            }
+
+            // Call the callback, bound to the numericinput
+            if ($.isFunction(callback)) {
+                callback.bind(numericinput)(numericinput);
+            }
+        });
+    }
+
+    // Set defaults for extension
+    $.fn.numericinput.defaults = {
+    }
+
+    // Numericinput implementation
+    function Numericinput(element, options) {
+        this.element = element;
+        this.options = options;
+    }
+
+    // Remove the numeric input properties; resets the input element to its former state
+    Numericinput.prototype.remove = function () {
+    }
+
+    // Event handlers should not be accessible from the object itself
+    let eventHandlers = {
+    };
+
+}(jQuery));
