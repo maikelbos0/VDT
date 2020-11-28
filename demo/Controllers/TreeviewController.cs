a194cbddd65bd7198545ca37b165a6f71735dacb
Maikel Bos <maikel.bos0@gmail.com>
2020-11-28 17:40:01 +0100

    Added page for first POC for treeview

---
+++ demo/Controllers/TreeviewController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace vdt.demo.Controllers {
+    [RoutePrefix("Treeview")]
+    public class TreeviewController : Controller {
+        [Route]
+        [Route("Index")]
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
