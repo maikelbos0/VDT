4ede0c8e6543be06a0a80217e9c476f88cdc62e2
Maikel Bos <maikel.bos0@gmail.com>
2020-12-03 20:16:14 +0100

    Basic tree test setup and some implementation

---
+++ jquery-treeview.tests/basic.js
@@ -0,0 +1,80 @@
+﻿describe('a basic treeview', function () {
+    it('can be created', function () {
+        var tree = $('#basic-treeview-created');
+
+        expect(tree.length).toEqual(1);
+        expect(function () {
+            tree.treeview({
+                data: [{
+                    value: '1',
+                    text: 'A value'
+                }]
+            });
+        }).not.toThrow();
+        expect(tree.data('treeview')).toBeTruthy();
+    });
+
+    it('hides the already present content', function () {
+        var tree = $('#basic-treeview-hides');
+
+        tree.treeview({
+            data: [{
+                value: '1',
+                text: 'A value'
+            }]
+        });
+
+        var hiddenContent = tree.find('.test-hidden');
+
+        expect($(hiddenContent[0]).css('display')).toEqual('none');
+        expect($(hiddenContent[1]).css('display')).toEqual('none');
+    });
+
+    it('needs required option "data"', function () {
+        var tree = $('#basic-treeview-data-required');
+
+        fail();
+    });
+
+    it('needs option "data" to be an array', function () {
+        var tree = $('#basic-treeview-data-array');
+
+        fail();
+    });
+
+    it('will add a list', function () {
+        var tree = $('#basic-treeview-list');
+
+        tree.treeview({
+            data: [{
+                value: '1',
+                text: 'A value'
+            }]
+        });
+        expect(tree.children('ul.treeview-list').length).toEqual(1);
+    });
+
+    it('will add list items', function () {
+        var tree = $('#basic-treeview-list-items');
+
+        fail();
+    });
+
+    it('will add togglers for list items with children', function () {
+        var tree = $('#basic-treeview-list-item-children-toggler');
+
+        fail();
+    });
+
+    it('will add child lists', function () {
+        var tree = $('#basic-treeview-child-list');
+
+        fail();
+    });
+
+    it('will add child list items', function () {
+        var tree = $('#basic-treeview-child-list-items');
+
+        fail();
+    });
+});
