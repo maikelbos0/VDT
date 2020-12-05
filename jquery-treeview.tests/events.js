1062bcafc2c9b461603fa41081416a12d8759494
Maikel Bos <maikel.bos0@gmail.com>
2020-12-05 08:40:56 +0100

    Add event test setup

---
+++ jquery-treeview.tests/events.js
@@ -0,0 +1,37 @@
+﻿describe('when a user', function () {
+    it('clicks on a toggler it hides the child items and changes the class', function () {
+        var tree = $('#treeview-events-toggler-click');
+
+        fail();
+    });
+
+    it('clicks on a closed toggler it shows the child items and changes the class', function () {
+        var tree = $('#treeview-events-toggler-click-open');
+
+        fail();
+    });
+
+    it('checks a checkbox it checks all descendants', function () {
+        var tree = $('#treeview-events-input-check-children');
+
+        fail();
+    });
+
+    it('unchecks a checkbox it unchecks all descendants', function () {
+        var tree = $('#treeview-events-input-uncheck-children');
+
+        fail();
+    });
+
+    it('checks a checkbox it checks ancestors as needed', function () {
+        var tree = $('#treeview-events-input-check-parent');
+
+        fail();
+    });
+
+    it('unchecks a checkbox it unchecks all ancestors', function () {
+        var tree = $('#treeview-events-input-uncheck-parent');
+
+        fail();
+    });
+});
