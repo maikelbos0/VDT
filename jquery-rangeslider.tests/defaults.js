ad79f61de66006181a28935b32e44c70d340ce4d
Maikel Bos <maikel.bos0@gmail.com>
2020-12-10 20:45:52 +0100

    Defaults tests

---
+++ jquery-rangeslider.tests/defaults.js
@@ -0,0 +1,25 @@
+﻿describe('rangeslider defaults', function () {
+    $.fn.rangeslider.defaults.getValue = function () {
+        return 30;
+    };
+
+    it('can be changed', function () {
+        var slider = $('#rangeslider-defaults');
+
+        slider.rangeslider();
+
+        expect(slider.find('input').val()).toEqual('30');
+    });
+
+    it('can still be overridden in the options after being changed', function () {
+        var slider = $('#rangeslider-defaults-override');
+
+        slider.rangeslider({
+            getValue: function () {
+                return 50;
+            }
+        });
+
+        expect(slider.find('input').val()).toEqual('50');
+    });
+});
