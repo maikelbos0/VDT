6db9aa8fcc598e33ad1d0124fe1ebd122592996a
Maikel Bos <maikel.bos0@gmail.com>
2020-12-10 20:52:12 +0100

    Defaults tests

---
+++ jquery-numericinput.tests/defaults.js
@@ -0,0 +1,31 @@
+﻿describe('numericinput defaults', function () {
+    $.fn.numericinput.defaults.getGroupSeparator = function () {
+        return '^';
+    };
+
+    it('can be changed', function () {
+        var input = $('#input-defaults');
+
+        input.numericinput();
+
+        input.val('1234');
+        input.change();
+
+        expect(input.val()).toEqual('1^234.00');
+    });
+
+    it('can still be overridden in the options after being changed', function () {
+        var input = $('#input-defaults-override');
+
+        input.numericinput({
+            getGroupSeparator: function () {
+                return '`';
+            }
+        });
+
+        input.val('1234');
+        input.change();
+
+        expect(input.val()).toEqual('1`234.00');
+    });
+});
