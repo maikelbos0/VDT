8be434678a7cbe1306daf3e50bcf6f4c0ae107fb
Maikel Bos <maikel.bos0@gmail.com>
2020-12-11 17:34:42 +0100

    First event tests on treeview

--- jquery-treeview.tests/events.js
+++ jquery-treeview.tests/events.js
@@ -2,13 +2,62 @@
     it('clicks on a toggler it hides the child items and changes the class', function () {
         var tree = $('#treeview-events-toggler-click');
 
-        fail();
+        tree.treeview({
+            data: [{
+                value: '1',
+                text: 'Foo'
+            },
+            {
+                value: '2',
+                text: 'Bar',
+                children: [
+                    {
+                        value: '3',
+                        text: 'Baz'
+                    },
+                    {
+                        value: '4',
+                        text: 'Quux'
+                    }
+                ]
+            }]
+        });
+
+        tree.find('li:nth-child(2) > .treeview-toggler').click();
+
+        expect(tree.find('li:nth-child(2) > .treeview-toggler').hasClass('treeview-toggler-closed')).toEqual(true);
+        expect(tree.find('li ul').css('display')).toEqual('none');
     });
 
     it('clicks on a closed toggler it shows the child items and changes the class', function () {
         var tree = $('#treeview-events-toggler-click-open');
 
-        fail();
+        tree.treeview({
+            data: [{
+                value: '1',
+                text: 'Foo'
+            },
+            {
+                value: '2',
+                text: 'Bar',
+                children: [
+                    {
+                        value: '3',
+                        text: 'Baz'
+                    },
+                    {
+                        value: '4',
+                        text: 'Quux'
+                    }
+                ]
+            }]
+        });
+
+        tree.find('li:nth-child(2) > .treeview-toggler').click();
+        tree.find('li:nth-child(2) > .treeview-toggler').click();
+
+        expect(tree.find('li:nth-child(2) > .treeview-toggler').hasClass('treeview-toggler-closed')).toEqual(false);
+        expect(tree.find('li ul').css('display')).not.toEqual('none');
     });
 
     it('checks a checkbox it checks all descendants', function () {
