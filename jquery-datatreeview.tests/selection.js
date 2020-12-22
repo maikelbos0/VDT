bf65323e1ade3e0c0b5c6f761c74e2056c98a508
Maikel Bos <maikel.bos0@gmail.com>
2020-12-22 20:37:55 +0100

    Selection tests first setup

---
+++ jquery-datatreeview.tests/selection.js
@@ -0,0 +1,8 @@
+﻿describe('in a datatreeview a user can select', function () {
+    // TODO move any selection tests here
+    // TODO add new tests for freehand selection
+
+    it('something but the tests are not yet written', function () {
+        fail();
+    });
+});
